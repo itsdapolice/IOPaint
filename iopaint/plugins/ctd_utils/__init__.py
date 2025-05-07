f84af195577c6c32b94ecccf9792174abadedcd4
itsdapolice <163411553+itsdapolice@users.noreply.github.com>
2025-05-08 00:57:00 +0100

    Add files via upload

--- iopaint/plugins/ctd_utils/__init__.py
+++ iopaint/plugins/ctd_utils/__init__.py
@@ -1,6 +1,6 @@
 #from .basemodel import TextDetBase, TextDetBaseDNN
 from .basemodel import TextDetBaseDNN
-from .utils.yolov5_utils import non_max_suppression
+#from .utils.yolov5_utils import non_max_suppression
 from .utils.db_utils import SegDetectorRepresenter
 from .utils.imgproc_utils import letterbox
 from .textmask import refine_mask, refine_undetected_mask, REFINEMASK_INPAINT, REFINEMASK_ANNOTATION
